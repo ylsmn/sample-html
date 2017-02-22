9cda136e3802cf476008d65e1922b09e472465ca
Yosua Lesmana Sitepu <Yosua.LesmanaSitepu@mitrais.com>
2017-02-22 17:57:03 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,8 +4,8 @@ import groovy.json.JsonSlurperClassic
 node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 	
-	def applicationName = 'fajar-app' //change me
-	def deploymentGroupName = 'fajar-app' // change me
+	def applicationName = 'diyon-app' //change me
+	def deploymentGroupName = 'diyon-yosua-app' // change me
 	def s3BucketName = 'deployment-cdc'
 	
 	//Cleanup workspace
